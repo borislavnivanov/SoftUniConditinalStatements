c84f7caa776319fa0f1d74d4ea47a5dff0963032
Borislav Ivanov <borislavnivanov@gmail.com>
2024-02-12 13:09:36 +0200

    update

--- Exercise/01. Sum Seconds.py
+++ Exercise/01. Sum Seconds.py
@@ -9,7 +9,9 @@ total_times = time_first + time_second + time_third
 minutes = total_times // 60
 seconds = total_times % 60
 
-if seconds < 10:
-    print(f'{minutes}:0{seconds}')
-else:
-    print(f'{minutes}:{seconds}')
+print(f'{minutes}:{seconds:02d}')
+
+# if seconds < 10:
+#     print(f'{minutes}:0{seconds}')
+# else:
+#     print(f'{minutes}:{seconds}')
